b18369630685183309b39389b0af949f68fd598a
Deco-correa <54818184+Deco-correa@users.noreply.github.com>
2019-11-18 17:13:33 -0300

    Update lezcodeprojeto.py

--- lezcodeprojeto.py
+++ lezcodeprojeto.py
@@ -9,13 +9,18 @@ from bs4 import BeautifulSoup
 import requests
 empresa = input("nome da empresa:")
 url1 = f"https://finance.yahoo.com/quote/{empresa}?p={empresa}&.tsrc="
-r = req
-uests.get(url1)
+r = requests.get(url1)
 soup = BeautifulSoup(r.text, 'html.parser')
-last = soup.find("span",{"data-reactid":"44"})
-opn = soup.find("span",{"data-reactid":"49"})
-PE = soup.find("span",{"data-reactid":"95"})
-beta = soup.find("span",{"data-reactid":"110"})
-
-print(opn)
-print("hello")
+last = soup.find("td",{"data-test":"PREV_CLOSE-value"})
+opn = soup.find("td",{"data-test":"OPEN-value"})
+PE = soup.find("td",{"data-test":"PE_RATIO-value"})
+beta = soup.find("td",{"data-test":"BETA_3Y-value"})
+div = soup.find("td",{"data-test":"DIVIDEND_AND_YIELD-value"})
+print(f"forward dividend & yield =  {div.text}")
+print(f"previous close           =  {last.text}")
+print(f"price earnings           =  {PE.text}")
+print(f"beta                     =  {beta.text}")	
+print(f"open                     =  {opn.text}")
+#dontpad (link=sublimelc)
+#adicionar os mesmos critérios do S&P por setor, adicionar lista de empresas separadas por setor, comparar a empresa em questão
+#com resto do S&P e lista.
